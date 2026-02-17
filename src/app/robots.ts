58f6e7f9d5bb3b6b62baae8cf93365b7dec34ff3
gyrow32 <gyrow32@gmail.com>
2026-02-17 06:36:07 -0500

    [fix] update robots.txt and sitemap to clockoutnow.com (was getyourtimeback.com)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://getyourtimeback.com/sitemap.xml',
+    sitemap: 'https://clockoutnow.com/sitemap.xml',
   }
 }
